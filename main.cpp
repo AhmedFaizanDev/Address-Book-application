ffaf2d8211b59da4c229ccbb04e2ac4a2f9a53b5
AhmedFaizanDev <info2faizanahmed@gmail.com>
2024-07-17 12:42:24 +0530

    Implement add contact feature and store contacts in a vector

--- main.cpp
+++ main.cpp
@@ -1,31 +1,55 @@
-// Contact.h
-#ifndef CONTACT_H
-#define CONTACT_H
-
-#include <string>
+// main.cpp
+#include <iostream>
+#include <vector>
+#include "Contact.h"
 
 using namespace std;
 
-class Contact {
-private:
-    string name;
-    string phoneNumber;
-    string email;
+void displayMenu() {
+    cout << "Simple Address Book" << endl;
+    cout << "1. Add Contact" << endl;
+    cout << "2. Search Contact" << endl;
+    cout << "3. Display All Contacts" << endl;
+    cout << "4. Exit" << endl;
+    cout << "Enter your choice: ";
+}
 
-public:
-    // Constructor
-    Contact(string name, string phoneNumber, string email)
-        : name(name), phoneNumber(phoneNumber), email(email) {}
+void addContact(vector<Contact>& contacts) {
+    string name, phoneNumber, email;
+    cout << "Enter name: ";
+    cin >> name;
+    cout << "Enter phone number: ";
+    cin >> phoneNumber;
+    cout << "Enter email: ";
+    cin >> email;
+    contacts.emplace_back(name, phoneNumber, email);
+    cout << "Contact added successfully!" << endl;
+}
 
-    // Getters
-    string getName() const { return name; }
-    string getPhoneNumber() const { return phoneNumber; }
-    string getEmail() const { return email; }
+int main() {
+    vector<Contact> contacts;
+    int choice;
+    do {
+        displayMenu();
+        cin >> choice;
 
-    // Setters
-    void setName(const string& name) { this->name = name; }
-    void setPhoneNumber(const string& phoneNumber) { this->phoneNumber = phoneNumber; }
-    void setEmail(const string& email) { this->email = email; }
-};
+        switch (choice) {
+            case 1:
+                addContact(contacts);
+                break;
+            case 2:
+                cout << "Search Contact selected" << endl;
+                break;
+            case 3:
+                cout << "Display All Contacts selected" << endl;
+                break;
+            case 4:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice, please try again." << endl;
+        }
+    } while (choice != 4);
 
-#endif
+    return 0;
+}
